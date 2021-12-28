9af13de4a655e82d2a52ea42c23ebdca9e4dfe20
Readpato <87768087+Readpato@users.noreply.github.com>
2021-12-28 15:36:05 +0100

    test: Create test that selects a wrong combination
    The test that will assess if the program runs correctly when two wrong combinations are selected has been created.

--- cypress/integration/memotest.js
+++ cypress/integration/memotest.js
@@ -5,13 +5,14 @@ context("Memotest Game", () => {
     cy.visit(URL);
   });
 
-  describe("Memotest Game - Layout", () => {
+  const COLOR_BOX_QUANTITY = 12;
+
+  describe("Layout", () => {
     it("Assess that there is an existing gameboard", () => {
       cy.get(".game-board").should("exist");
     });
 
     it("Make sure that the right amount of color boxes exist", () => {
-      const COLOR_BOX_QUANTITY = 12;
       cy.get(".game-board")
         .find(".color-box")
         .should("have.length", COLOR_BOX_QUANTITY);
@@ -37,8 +38,40 @@ context("Memotest Game", () => {
       });
     });
 
-    it("Determine that when a color box is clicked, it will highlight", () => {
+    it("Determine that when a color box is clicked it will highlight itself", () => {
       cy.get(".color-box").eq(0).click().should("have.css", "opacity", "1");
     });
   });
+
+  describe("Running the game", () => {
+    let pairsMap, pairsList;
+    it("Selects a wrong combination", () => {
+      cy.visit(URL);
+      cy.get(".color-box").then((colorBoxes) => {
+        pairsMap = getColorBoxPair(colorBoxes);
+        pairsList = Object.values(pairsMap);
+        cy.get(pairsList[0][0]).click();
+        cy.get(pairsList[1][0]).click();
+        cy.get(".color-box").should("have.length", COLOR_BOX_QUANTITY);
+      });
+    });
+  });
 });
+
+function getColorBoxPair(colorBoxes) {
+  let pairs = {}; // *This is called a "Dictionary"
+
+  colorBoxes.each((i, colorBox) => {
+    const colorClass = colorBox.className.replace("color-box ", "");
+
+    // *If the pairs.colorClass property is not empty, then push the element into the array
+    if (pairs[colorClass]) {
+      pairs[colorClass].push(colorBox);
+    } else {
+      // *If the pairs.colorClass property IS then start an array with that element inside of it.
+      pairs[colorClass] = [colorBox];
+    }
+  });
+
+  return pairs;
+}
