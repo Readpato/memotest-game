e9257824e427299c9fda6df6af99220d36260608
Readpato <87768087+Readpato@users.noreply.github.com>
2021-12-28 14:37:50 +0100

    chore: Format code with Prettier
    The context function has been added to the main integration test file, which is going to help us to contain and describe properly next tests.
    
    Prettier has been re activated as the default text editor.

---
+++ cypress/integration/memotest.js
@@ -0,0 +1,44 @@
+const URL = "http://192.168.0.134:8080";
+
+context("Memotest Game", () => {
+  before(() => {
+    cy.visit(URL);
+  });
+
+  describe("Memotest Game - Layout", () => {
+    it("Assess that there is an existing gameboard", () => {
+      cy.get(".game-board").should("exist");
+    });
+
+    it("Make sure that the right amount of color boxes exist", () => {
+      const COLOR_BOX_QUANTITY = 12;
+      cy.get(".game-board")
+        .find(".color-box")
+        .should("have.length", COLOR_BOX_QUANTITY);
+    });
+
+    it("Determine if the color boxes are correctly randomized", () => {
+      cy.get(".color-box").then((previousColorBoxes) => {
+        let originalClassNames = [];
+        previousColorBoxes.each(function (index, colorBox) {
+          originalClassNames.push(colorBox.className);
+        });
+
+        cy.visit(URL);
+
+        cy.get(".color-box").then((newColorBoxes) => {
+          let newClassNames = [];
+          newColorBoxes.each(function (index, colorBox) {
+            newClassNames.push(colorBox.className);
+          });
+
+          cy.wrap(originalClassNames).should("not.deep.equal", newClassNames);
+        });
+      });
+    });
+
+    it("Determine that when a color box is clicked, it will highlight", () => {
+      cy.get(".color-box").eq(0).click().should("have.css", "opacity", "1");
+    });
+  });
+});
